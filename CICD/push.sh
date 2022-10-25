95f48cbf4feecf79aef33a929778daaec5a37370
ladobence <ladomerszky.bence@gmail.com>
2022-10-25 22:08:13 +0200

    push script added

---
+++ CICD/push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+cd projectdir
+git add .
+set +e  # Grep succeeds with nonzero exit codes to show results.
+git status | grep modified
+if [ $? -eq 0 ]
+then
+    set -e
+    git push https://github.com/SandorBalazsHU/elte-ik-msc-weather-cam 
+else
+    set -e
+    echo "No changes since last run"
+fi
+
