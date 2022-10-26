bfcc0e21736d29a8ae1e20d539460e4f725e3b2e
MarcellHarmaci <harmaci.marci@gmail.com>
2022-10-26 23:42:02 +0200

    Backend base

---
+++ WebAPI/src/include/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+const PROJECT_ROOT_PATH = __DIR__ . "/../";
+ 
+require_once PROJECT_ROOT_PATH . "/include/db-config.php";
+require_once PROJECT_ROOT_PATH . "/controller/BaseController.php";
+require_once PROJECT_ROOT_PATH . "/dataSource/DatabaseAccessObject.php";
---
+++ WebAPI/src/include/db-config.php
@@ -0,0 +1,6 @@
+<?php
+
+const DB_HOST = "localhost";
+const DB_USERNAME = "root";
+const DB_PASSWORD = "Kigy000~";
+const DB_DATABASE_NAME = "sbxcom_weather";
